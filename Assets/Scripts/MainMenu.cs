cf0688b5d56626968d8bb7f74cfa394ae322fe85
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-10-19 11:32:32 -0400

    Minor Main Menu bug fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
