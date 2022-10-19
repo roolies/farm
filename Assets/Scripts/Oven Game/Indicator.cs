0f96219ff985dc33868259c001f9d070d822e7aa
Marcus0077 <marcus.werner@knights.ucf.edu>
2022-10-18 22:31:54 -0400

    Added Oven Minigame
    Colliding with the red circle opens the oven game. Press A to stop the capsule and B to return to the main scene.

--- Assets/Scripts/Oven Game/Indicator.cs
+++ Assets/Scripts/Oven Game/Indicator.cs
@@ -1,19 +1,55 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
  
 public class Indicator : MonoBehaviour
 {
     public Vector3 pointB;
+    private bool active = true;
+
+    FarmerGame ovenControls;
+    private InputAction stopIndicator;
+    private InputAction exitOven;
+
+    public TextMeshProUGUI result;
    
     IEnumerator Start()
     {
         transform.position = new Vector3(-8.0f, 1.5f, 0f);
 
         var pointA = transform.position;
-        while (true)
+        while (active)
         {
-            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 1.0f));
-            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 1.0f));
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.7f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 0.7f));
+        }
+    }
+
+    public void Update()
+    {
+        if (exitOven.IsPressed())
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+        if (active == false)
+        {
+            if (Math.Abs(transform.position.x) <= 0.5)
+            {
+                result.text = "Your dish is perfect!";
+            }
+            else if (Math.Abs(transform.position.x) <= 3.0)
+            {
+                result.text = "Your dish is slightly overcooked!";
+            }
+            else if (Math.Abs(transform.position.x) <= 8.0)
+            {
+                result.text = "Your dish is burnt!";
+            }
         }
     }
    
@@ -23,6 +59,11 @@ public class Indicator : MonoBehaviour
         var rate = 1.0f/time;
         while (i < 1.0f)
         {
+            if (stopIndicator.IsPressed())
+            {
+                active = false;
+                break;
+            }
 
             i += Time.deltaTime * rate;
             thisTransform.position = Vector3.Lerp(startPos, endPos, i);
@@ -32,6 +73,18 @@ public class Indicator : MonoBehaviour
 
     private void OnEnable()
     {
+        ovenControls = new FarmerGame();
+        
+        stopIndicator = ovenControls.Oven.StopIndicator;
+        stopIndicator.Enable();
 
+        exitOven = ovenControls.Oven.ExitOven;
+        exitOven.Enable();
+    }
+
+    private void OnDisable()
+    {
+        stopIndicator.Disable();
+        exitOven.Disable();
     }
 }
