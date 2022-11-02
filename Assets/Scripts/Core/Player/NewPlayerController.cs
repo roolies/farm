73c1b70547b1274466f94067fa6051b354415d72
CroissantToes <cteles20@gmail.com>
2022-11-02 02:27:45 -0400

    Sfx + cat

--- Assets/Scripts/Core/Player/NewPlayerController.cs
+++ Assets/Scripts/Core/Player/NewPlayerController.cs
@@ -9,8 +9,14 @@ public class NewPlayerController : MonoBehaviour
     public Vector2 moveInput = Vector2.zero;
     Rigidbody2D rb;
 
+    public AudioClip eggSound;
+    public AudioClip turkeySound;
+
+    private AudioSource audioSource;
+
     void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
@@ -23,5 +29,19 @@ public class NewPlayerController : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Turkey"))
+        {
+            audioSource.clip = turkeySound;
+            audioSource.Play();
+        }
+        else if(collision.CompareTag("Egg"))
+        {
+            audioSource.clip = eggSound;
+            audioSource.Play();
+        }
     }
 }
